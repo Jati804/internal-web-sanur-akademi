15349d2646eabeff13e0e23f625c95259ac5fdcf
Jati804 <jatiwesi22@gmail.com>
2026-01-25 11:58:51 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service Worker untuk PWA SANUR
+const CACHE_NAME = 'sanur-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
