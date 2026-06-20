e44b5f19e63f9ffe9cd0f5f395947d8bfdc5e006
Jati804 <jatiwesi22@gmail.com>
2026-06-20 20:13:20 +0700

    Update TeacherDashboard.tsx

--- pages/TeacherDashboard.tsx
+++ pages/TeacherDashboard.tsx
@@ -562,12 +562,12 @@ setTeacherInputValue(editData.teacherId !== user.id ? (teachers.find(t => t.id =
                       Kelas ini sedang berjalan oleh:
                     </p>
                     <div className="bg-white rounded-xl px-4 py-2">
-                      <p className="text-[11px] font-black text-slate-800 uppercase tracking-wide leading-relaxed">{blockModal.className}</p>
+                      <p className="text-[10px] font-black text-slate-800 uppercase tracking-wide leading-relaxed">{blockModal.className}</p>
                     </div>
                     <div className="bg-white rounded-xl px-4 py-3 space-y-1">
-                      <p className="text-[9px] font-bold text-rose-600 uppercase tracking-wide">Guru:</p>
-                      <p className="text-[11px] font-black text-slate-800 uppercase">{blockModal.ownerName}</p>
-                      <p className="text-[9px] font-bold text-rose-600">Sesi {blockModal.currentSession} / 6</p>
+                      <p className="text-[10px] font-bold text-rose-600 uppercase tracking-wide">Guru:</p>
+                      <p className="text-[10px] font-black text-slate-800 uppercase">{blockModal.ownerName}</p>
+                      <p className="text-[10px] font-bold text-rose-600">Sesi {blockModal.currentSession} / 6</p>
                     </div>
                   </div>
                 </div>
