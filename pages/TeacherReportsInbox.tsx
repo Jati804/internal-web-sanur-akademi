4a4e1f569e6b9cdbbadc356b4148020f73ab0340
Jati804 <jatiwesi22@gmail.com>
2026-09-20 14:20:48 +0700

    Update TeacherReportsInbox.tsx

--- pages/TeacherReportsInbox.tsx
+++ pages/TeacherReportsInbox.tsx
@@ -381,7 +381,7 @@ const handleDownloadPDF = async (req: any) => {
             <div style="width:100%; height:4px; background:${isPass ? '#dbeafe' : '#ffedd5'}; margin-top:10px; border-radius:10px;"></div>
           </div>
           <p style="font-size:14px; font-family:serif; font-style:italic; color:#475569; line-height:1.7; margin:0 0 8px 0; padding:0 100px;">${isPass ? 'Telah menyelesaikan seluruh materi pelatihan dan lulus dalam ujian standar kompetensi' : 'Telah berkomitmen mengikuti dan menyelesaikan seluruh rangkaian program pelatihan'}</p>
-          <p style="font-size:13px; font-family:serif; font-style:normal; color:${mainColor}; font-weight:600; letter-spacing:0.15em; margin:0 0 40px 0;">Sanur Akademi Inspirasi</p>
+          <p style="font-size:13px; font-family:serif; font-style:normal; color:${mainColor}; font-weight:600; letter-spacing:0.15em; margin:0 0 40px 0;">SANUR Akademi Inspirasi</p>
           <div style="background:${gradientBox}; width:700px; padding:30px 20px; border-radius:35px; display:flex; flex-direction:column; align-items:center; justify-content:center; box-shadow:0 12px 30px -8px rgba(0,0,0,0.15); margin-bottom:50px;">
             <p style="font-size:22px; font-weight:900; color:white; text-transform:uppercase; font-style:italic; margin:0; line-height:1.2;">${subject}</p>
             <p style="font-size:16px; font-weight:900; color:rgba(255,255,255,0.8); text-transform:uppercase; letter-spacing:0.3em; margin:8px 0 0 0;">LEVEL ${level}</p>
